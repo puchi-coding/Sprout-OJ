377984e79d0e84c901cd25cd3af11ef67413323f
puchi-coding <woodrow1118nthu@gmail.com>
2024-05-11 13:39:16 +0800

    2024/05/11

--- C++/No.6002 小普敲鍵盤.cpp
+++ C++/No.6002 小普敲鍵盤.cpp
@@ -1,8 +1,9 @@
-// Sprout OJ No.6002-普敲鍵盤
+// Sprout OJ No.6002-小普敲鍵盤
 #include <iostream>
 using namespace std;
 int main() {
   cout<<"qwertyuiop[]\\"<<endl;
   cout<<"asdfghjkl;\'"<<endl;
   cout<<"zxcvbnm,./"<<endl;
+  return 0;
 }
